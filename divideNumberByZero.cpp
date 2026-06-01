50c4f5dd3b54c97a57cb306ff74da3be71bb9bfe
idrumbetter <margolismn110@mail.sandhills.edu>
2026-06-01 19:41:25 -0400

    I'm committing the bulk of lab 1 for week 2

---
+++ divideNumberByZero.cpp
@@ -0,0 +1,28 @@
+/*
+@author: Michael Margolis 
+@date: 06/01/2026
+@purpose: This program takes an integer and divides it by zero
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declaring the variable
+    int num, result;
+
+    // Capturing the integer to be divided by zero
+    cout << "What number would you like to divide by 0? ";
+    cin >> num;
+
+    // dividing by 0
+    result = num / 0;
+
+    // displaying the result
+    cout << "The result of dividing " << num << " by 0 is " << result;
+
+    return 0;
+
+} 
