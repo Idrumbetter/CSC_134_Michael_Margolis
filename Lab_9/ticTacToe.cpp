40fc124bb87aebed9f3d2ba64c69556cfd1a5298
idrumbetter <margolismn110@mail.sandhills.edu>
2026-07-20 16:42:28 -0400

    notes

--- Lab_9/ticTacToe.cpp
+++ Lab_9/ticTacToe.cpp
@@ -12,59 +12,98 @@ using namespace std;
 
 // the main function beginning
 int main() {
+    // setting up a constant variable for the number of turns to lock the amount of data an array can hold
     const int SIZE = 9;
+    // setting up the tic tac toe board display as an array
     string board[25] = {"   ", "|", "   ", "|", "   ",
                         "---", "+", "---", "+", "---",
                         "   ", "|", "   ", "|", "   ",
                         "---", "+", "---", "+", "---",
-                        "   ", "|", "   ", "|", "   "
-    };
+                        "   ", "|", "   ", "|", "   "};
+
+    //  setting up the menu of options for the player
     string playerOptionsMenu[SIZE] = {"1. Top Left", "2. Top Middle", "3. Top Right",
                                   "4. Middle Left", "5. Middle Middle", "6. Middle Right",
                                   "7. Bottom Left", "8. Bottom Middle", "9. Bottom Right"};
 
+    // setting up a check for the players check list for if the input already existed
     string playerChoiceCheck[SIZE] = {"1", "2", "3",
                                     "4", "5", "6",
                                     "7", "8", "9"};
+    
+    // setting up a character switching variable
     char player = '1';
+    
+    // setting up a game decision history to store the decisions of both players
     string playerChoices[SIZE] = {};
-    int choiceCount = 0;
-    bool win = false;
 
+    // printing an opening statement for beginning the match
     cout << "Welcome to tic-tac-toe. Are you ready to duel!\n\n";
+
+    // printing the board to the console to initate the game by running through the board array
     for (int n = 0; n < 25; ++n) {
+        // this checks if we need to move to the next like so the game lines up properly
         if (n == 5 || n== 10 || n==15 || n==20) {
             cout << endl;
         }
+    // printing the board based on the nth index position
     cout << board[n];
     }
 
-
+    // setting a for loop for the max number of turns a game of tic tac toe can go
     for (int x=0; x<9;++x) {
+
+        // setting a boolean variable to relay if the input is a valid choice or not
         bool validChoice = false;
+
+        // setting a boolean variable to relay if the choice has already been used in the past
         bool spotTaken = false;
+
         cout << endl;
+
+        // setting a for loop to show what options are left available for the player
         for (int y=0; y < SIZE; ++y) {
+            // setting a boolean variable that stores if a match was found
             bool foundYah = false;
+
+            // setting a for loop to run through every indexed position of the playerChoiceCheck array for comparison
             for (int z = 0; z<SIZE;++z) {
+                // an if statement the sets foundYah to true if it matches any position in the playerChoices array
                 if (playerChoiceCheck[y] == playerChoices[z]) {
                     foundYah = true;
                 }
             }
+            // prints the indexed position value if the match was never found
             if (foundYah == false) {
                 cout << playerOptionsMenu[y] << endl;
             }
         }
+
+        // checks which player's move we are on
         if (player == '1') {
+            // printing a message so the players know which term it is
             cout << "\nPlayer one:\n\n";
+
+            // setting a while loop so the game doesn't move on until a valid move is picked by the player
             while (validChoice == false) {
+                // setting a varianble to hold the input until it's confirmed to be valid
                 string currentInput = "";
+
+                // resetting spotTaken to false each loop through the while loop
                 spotTaken = false;
+
+                // asking user for which move they want to make
                 cout << "Where would you like to go? ";
                 cin >> currentInput;
+
+                // setting a for loop to make sure the input didn't already get used
                 for (int w = 0;w<x;++w) {
                     if (currentInput == playerChoices[w]) { spotTaken = true;}
                 }
+
+                // if input was a new position value then we update the board in the specific position,   //
+                // turn validChoice true to break free of the while loop, and permenantly saves the input //
+                // to playerChoices                                                                       //
                 if (spotTaken == false) {
                 if (currentInput == "1") {board[0] = " O "; validChoice = true; playerChoices[x]=currentInput;}
                 if (currentInput == "2") {board[2] = " O "; validChoice = true; playerChoices[x]=currentInput;}
@@ -75,15 +114,18 @@ int main() {
                 if (currentInput == "7") {board[20] = " O "; validChoice = true; playerChoices[x]=currentInput;}
                 if (currentInput == "8") {board[22] = " O "; validChoice = true; playerChoices[x]=currentInput;}
                 if (currentInput == "9") {board[24] = " O "; validChoice = true; playerChoices[x]=currentInput;}
+                // this if statement is for any input that is completely out of the scope of the game
                 if (currentInput != "1" && currentInput != "2" && currentInput != "3" && 
                     currentInput != "4" && currentInput != "5" && currentInput != "6" && 
                     currentInput != "7" && currentInput != "8" && currentInput != "9") {
                         cout << "Invalid choice. Please try again";
                     }
                 cout << endl;
+                // if the spotTaken variable is true this will be displayed
             } else {cout << "Spot was already taken. Please Try Again\n";}
         }
-            player = '2';
+        // switch which player is the active one
+        player = '2';
         }
         else {
             cout << "\nPlayer two:\n\n";
